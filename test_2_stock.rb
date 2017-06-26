b0714b7cb5955a6fd23d25a8a2d6b0f58f1c12f3
John <bramleyjl@gmail.com>
2017-06-26 23:39:23 +0800

    refactored stock picker program

--- test_2_stock.rb
+++ test_2_stock.rb
@@ -2,29 +2,25 @@ def stock_picker(input)
 
 	profits = []
 	dates = Hash.new
+
 	input.each do |price|
-
-		x = -1
-		
+		x = -1	
 		input.count.times do
-
 			if (x == -1 || x > input.index(price)) && input[x] - price > 0
-				
 				profits.push(input[x] - price)
-				dates[input[x] - price] = "#{price} -> #{input[x]}"
-
+				dates[input[x] - price] = [price, input[x]]
 				x += 1
-
 			else
 				x += 1
-			end
-			
-		end 
+			end		
+		end	
 	end
-puts profits.sort[-1]
-puts dates.fetch(profits.sort[-1])
+
+puts "Profit: $#{profits.sort[-1]}"
+puts "Buy at $#{dates.sort[-1][-1][0]}"
+puts "Sell at $#{dates.sort[-1][-1][-1]}"
 
 end
 
-stock_picker([6, 7, 10, 1, 9, 5])
+stock_picker([6, 7, 21, 1, 10, 17])
 
